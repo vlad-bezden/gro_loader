9c1c06b43561bfd2694062520b65a606cabf7d57
Vlad Bezden <vlad.bezden@gmail.com>
2020-07-16 21:06:20 -0400

    Make program more flexible with adding more flags
    Add Output directory to command line
    Add ps pr parameters and changed -config flag
    Update documentation

--- README.md
+++ README.md
@@ -2,15 +2,31 @@
 
 ## Usage
 
-### From go command line
+### Parameters
+Only -s start (date or date time) and -e end (date or date time) parameters are required.
+
+To view all available flags use:
+
+```
+gro_loader.exe help
+```
+
+### Run from go command line
 ```
 go run main.go -s 2020-07-06 -e 2020-07-10
 ```
 
 ### Executable
 
+#### Dates only
 ```
 gro_loader.exe -s 2020-07-06 -e 2020-07-10
+
+```
+
+#### Date Time
+```
+gro_loader.exe -s 2020-07-06T08:00:00 -e 2020-07-10T23:59:59
 ```
 
 ## Request Required Parameters
@@ -56,13 +72,33 @@ gro_loader.exe -s 2020-07-06 -e 2020-07-10
 ```
 
 ## Config information
-- URL
 - User ID
 - Process Session ID
 - Security Token
 - Local File Path
 
 ## Command line flags
- - (-c) Config File path (optional). Default value ./config.json
- - (-s) Start Date
- - (-e) End Date
+
+```
+  -c string
+        Configuration file path. (default "./config.json")
+
+  -e string
+        End DateTime in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH-MM-SS' format. (Required)
+  -o string
+        Output file path (default "./data.json")
+  -pr int
+        The number of pages requested; this is the number that should be changed in
+        the case there are more than 10 queries in a search your financial institution
+        is trying to utilize. For example, if the financial institution has 300
+        applications in the month of December (using the same time constraints from
+        before), then the URL parameter “pr” would be set to any value from 1-30
+        (taking the applications 10 entries at a time – 1-10, 11-20, 21-30, etc). (default 1)
+  -ps int
+        Stands for the size of the page.
+        Since the standard is having 10 applications
+        maximum per page (as seen in the admin portal search queries), we recommend
+        only using a maximum of 10 for this value in the URL. (default 10)
+  -s string
+        Start DateTime in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH-MM-SS' format. (Required)
+```
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"text/template"
 )
 
@@ -18,14 +20,16 @@ type config struct {
 	UserID             string
 	ProcessorSessionID string
 	SecurityToken      string
-	OutputFilePath     string
 }
 
 //flag provides command line parameters
 type flags struct {
-	ConfigFilePath string
-	StartDateTime  string
-	EndDateTime    string
+	startDateTime  string
+	endDateTime    string
+	configFilePath string
+	outputFilePath string
+	pr             int
+	ps             int
 }
 
 //bodyParams provides parameters for POST body template
@@ -39,9 +43,21 @@ type bodyParams struct {
 
 //getFlags gets command line flags, such as config file path, start and end date
 func getFlags() flags {
-	configPathPtr := flag.String("config", "./config.json", "Configuration file path.")
-	startDTPtr := flag.String("s", "", "Start DateTime in 'YYYY-MM-DDTHH-MM-SS' format. (Required)")
-	endDTPtr := flag.String("e", "", "End DateTime in 'YYYY-MM-DDTHH-MM-SS' format. (Required)")
+	startDTPtr := flag.String("s", "", "Start DateTime in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH-MM-SS' format. (Required)")
+	endDTPtr := flag.String("e", "", "End DateTime in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH-MM-SS' format. (Required)")
+	prPtr := flag.Int("pr", 1, `The number of pages requested; this is the number that should be changed in
+the case there are more	than 10 queries in a search your financial institution
+is trying to utilize. For example, if the financial institution has 300
+applications in the month of December (using the same time constraints from
+before), then the URL parameter “pr” would be set to any value from 1-30
+(taking the applications 10 entries at a time – 1-10, 11-20, 21-30, etc).`)
+	psPtr := flag.Int("ps", 10, `Stands for the size of the page.
+Since the standard is having 10 applications
+maximum per page (as seen in the admin portal search queries), we recommend
+only using a maximum of 10 for this value in the URL.`)
+	configPathPtr := flag.String("c", "./config.json", "Configuration file path.")
+	outputFilePathPtr := flag.String("o", "./data.json", "Output file path")
+
 	flag.Parse()
 
 	if *startDTPtr == "" || *endDTPtr == "" {
@@ -49,7 +65,11 @@ func getFlags() flags {
 		os.Exit(1)
 	}
 
-	return flags{*configPathPtr, *startDTPtr, *endDTPtr}
+	return flags{*startDTPtr,
+		*endDTPtr,
+		*configPathPtr,
+		*outputFilePathPtr,
+		*prPtr, *psPtr}
 }
 
 func main() {
@@ -57,10 +77,10 @@ func main() {
 	// Command line flags
 	flags := getFlags()
 	// configuration parameters from config file
-	config := getConfigData(flags.ConfigFilePath)
+	config := getConfigData(flags.configFilePath)
 	// get body template parameters
 	bodyParams := bodyParams{config.UserID, config.ProcessorSessionID,
-		config.SecurityToken, flags.StartDateTime, flags.EndDateTime}
+		config.SecurityToken, flags.startDateTime, flags.endDateTime}
 
 	template, err := template.ParseFiles(`./bodytemplate.json`)
 	panicIfErr(err)
@@ -70,11 +90,14 @@ func main() {
 	panicIfErr(err)
 	log.Println("Created body content and ready to make a request")
 
-	req, err := http.NewRequest("POST", config.URL, bytes.NewBuffer(buff.Bytes()))
+	url := urlQueryString(config.URL, flags.pr, flags.ps)
+	log.Printf("URL Path %s", url)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(buff.Bytes()))
 	panicIfErr(err)
 	req.Header.Set("Content-Type", "application/json")
 
-	log.Printf("Sending request to the server: %v", config.URL)
+	log.Printf("Sending request to the server: %v", url)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	panicIfErr(err)
@@ -85,13 +108,13 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	out, err := os.Create(config.OutputFilePath)
+	out, err := os.Create(flags.outputFilePath)
 	panicIfErr(err)
 	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 	panicIfErr(err)
-	log.Printf("Successfully loaded and saved file to %s", config.OutputFilePath)
+	log.Printf("Successfully loaded and saved file to %s", flags.outputFilePath)
 }
 
 //getConfigData load configuration information from config file
@@ -108,6 +131,19 @@ func getConfigData(configFilePath string) config {
 	panicIfErr(err)
 
 	return config
+}
+
+//urlQueryString creates url with query string parameters
+func urlQueryString(baseURL string, pr, ps int) string {
+	u, err := url.Parse(baseURL)
+	panicIfErr(err)
+
+	q := u.Query()
+	q.Add("pr", strconv.Itoa(pr))
+	q.Add("ps", strconv.Itoa(ps))
+	u.RawQuery = q.Encode()
+
+	return u.String()
 }
 
 //panicIfErr prints error stack and exit application with error code 1
